5e3d94c7054565ad2bed02bece93b21bb9240bfa
avinashkethineedi <avinash.kethineedi@amd.com>
2024-11-05 22:50:58 +0000

    Update collective APIs to use teams interface
    * Use team-relative numbering in collective functions
    * Replace log_stride with stride

--- src/ipc/context_ipc_device_coll.cpp
+++ src/ipc/context_ipc_device_coll.cpp
@@ -101,13 +101,9 @@ __device__ void IPCContext::internal_sync(int pe, int PE_start, int stride,
 __device__ void IPCContext::sync(roc_shmem_team_t team) {
   IPCTeam *team_obj = reinterpret_cast<IPCTeam *>(team);
 
-  /**
-   * Ensure that the stride is a multiple of 2.
-   */
-  int log_pe_stride = static_cast<int>(team_obj->tinfo_wrt_world->log_stride);
   int pe = team_obj->my_pe_in_world;
   int pe_start = team_obj->tinfo_wrt_world->pe_start;
-  int pe_stride = (1 << log_pe_stride);
+  int pe_stride = team_obj->tinfo_wrt_world->stride;
   int pe_size = team_obj->num_pes;
 
   internal_sync(pe, pe_start, pe_stride, pe_size, barrier_sync);
