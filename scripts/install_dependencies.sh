785e31aa485f6c7ad1089e156f48cac7420f0598
Yiltan <ytemucin@amd.com>
2025-02-18 12:30:34 -0500

    Sync develop with amd-mainline (#46)
    * Update install_dependencies.sh
    
    * Updated to ROCm repos
    
    * Merge pull request #37 from ROCm/depBuild
    
    locked specific version on ompi and ucx
    
    * locked specific version on ompi and ucx
    
    * [IPC] Fix ROCSHMEM_SIGNAL_ADD
    
    * Generate CMake Package Configuration Files
    
    ---------
    
    Co-authored-by: akolliasAMD <99202231+akolliasAMD@users.noreply.github.com>
    Co-authored-by: akolliasAMD <akollias@amd.com>

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -8,25 +8,26 @@ if [[ -z "${_ROCM_DIR}" ]]; then
 fi
 
 # Location of dependencies source code
-export _INSTALL_DIR=$HOME/installDIR
+export _INSTALL_DIR=$BUILD_DIR/install
 export _DEPS_SRC_DIR=$_INSTALL_DIR/src
 
 mkdir -p $_DEPS_SRC_DIR
 
 #Adjust branches and installation location as necessary
 export _UCX_INSTALL_DIR=$_INSTALL_DIR/ucx
-export _UCX_REPO=https://github.com/openucx/ucx.git
-export _UCX_BRANCH=v1.17.x
+export _UCX_REPO=https://github.com/ROCm/ucx.git
+export _UCX_COMMIT_HASH=4ef9a097c12ee6f7a8d3e41c317ea2d47e424b32
 
 export _OMPI_INSTALL_DIR=$_INSTALL_DIR/ompi
-export _OMPI_REPO=https://github.com/open-mpi/ompi.git
-export _OMPI_BRANCH=v5.0.x
+export _OMPI_REPO=https://github.com/ROCm/ompi.git
+export _OMPI_COMMIT_HASH=8a5c2ef25dc8e4528f0d3fd2ec91a6578160af95
 
 # Step 1: Build UCX with ROCm support
 cd $_DEPS_SRC_DIR
 rm -rf ucx
-git clone $_UCX_REPO -b $_UCX_BRANCH
+git clone $_UCX_REPO
 cd ucx
+git checkout $_UCX_COMMIT_HASH
 ./autogen.sh
 ./contrib/configure-release --prefix=$_UCX_INSTALL_DIR \
                             --with-rocm=$_ROCM_DIR     \
@@ -42,8 +43,10 @@ make install
 # Step 2: Install OpenMPI with UCX support
 cd $_DEPS_SRC_DIR
 rm -rf ompi
-git clone --recursive $_OMPI_REPO -b $_OMPI_BRANCH
+git clone --recursive $_OMPI_REPO
 cd ompi
+git checkout $_OMPI_COMMIT_HASH
+git submodule update --init --recursive
 ./autogen.pl
 ./configure --prefix=$_OMPI_INSTALL_DIR  \
             --with-rocm=$_ROCM_DIR       \
@@ -63,7 +66,7 @@ rm -rf $_DEPS_SRC_DIR
 
 echo "Dependencies for rocSHMEM are now installed"
 echo ""
-echo "UCX $_UCX_BRANCH Installed to $_UCX_INSTALL_DIR"
-echo "OpenMPI $_OMPI_BRANCH Installed to $_OMPI_INSTALL_DIR"
+echo "UCX ($_UCX_COMMIT_HASH) Installed to $_UCX_INSTALL_DIR"
+echo "OpenMPI ($_OMPI_COMMIT_HASH) Installed to $_OMPI_INSTALL_DIR"
 echo ""
 echo "Please update your PATH and LD_LIBRARY_PATH"
